d1b4f80fab9023c85efb0c504cfb7fe54e24f820
Jay <jaypatel9779ca@gmail.com>
2024-11-01 22:19:30 -0400

    changes in create employee, update and delete api

--- Controller/employeeData.js
+++ Controller/employeeData.js
@@ -9,3 +9,37 @@ module.exports.getEmployeeData = async (req, res) => {
     res.status(500).send(error);
   }
 };
+
+module.exports.createEmployeeData = async (req, res) => {
+  try {
+    const empData = new Employee(req.body);
+    const eData = await empData.save();
+    res.status(200).send(eData);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+module.exports.updateEmployeeData = async (req, res) => {
+  try {
+    const id = req.body.id;
+    const empData = await Employee.findByIdAndUpdate(id, req.body, {
+      new: true,
+    });
+    res.status(200).send(empData);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
+
+module.exports.deleteEmployeeData = async (req, res) => {
+  try {
+    const id = req.body.id;
+    const delData = await Employee.findByIdAndDelete(id);
+    res
+      .status(200)
+      .send({ delData: delData, message: "delete emplyee successful.." });
+  } catch (error) {
+    res.status(500).send(error);
+  }
+};
